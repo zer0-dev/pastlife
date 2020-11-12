69d5637985e01ccfe985acf648c802a4a3fade2e
zer0-dev <sharatest@bk.ru>
2020-11-13 02:19:40 +0300

    Updates

---
+++ precache-manifest.6f41e4391f1f357780e144aff5e2e87f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "472c4da9a7a1da768ac520d08eddbb4a",
+    "url": "/pastlife/index.html"
+  },
+  {
+    "revision": "53ebb97db12e054a483f",
+    "url": "/pastlife/static/css/2.8cdfe4cf.chunk.css"
+  },
+  {
+    "revision": "53ebb97db12e054a483f",
+    "url": "/pastlife/static/js/2.6b2c2db8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/pastlife/static/js/2.6b2c2db8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4d5619ddc056d53d540",
+    "url": "/pastlife/static/js/main.3f83dcef.chunk.js"
+  },
+  {
+    "revision": "cda079f4fcd235c24819",
+    "url": "/pastlife/static/js/runtime-main.a454813d.js"
+  }
+]);
